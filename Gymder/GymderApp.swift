3ef8dd243b85abe88d38fe197f2c62778eee8f56
Vatsal <vatsalbaherwani@gmail.com>
2022-02-17 13:45:37 -0500

    Initial Commit

---
+++ Gymder/GymderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymderApp.swift
+//  Gymder
+//
+//  Created by Vatsal Baherwani on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GymderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
